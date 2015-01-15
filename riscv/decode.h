287d6d62466cad88bcc27015913225f53ec969f7
Isaac Evans <ine@mit.edu>
2015-01-15 01:10:16 +0000

    simplistically update regfile_t template to have tag

--- riscv/decode.h
+++ riscv/decode.h
@@ -44,29 +44,26 @@ const int NFPR = 32;
 #define FSR_NXA  (FPEXC_NX << FSR_AEXC_SHIFT)
 #define FSR_AEXC (FSR_NVA | FSR_OFA | FSR_UFA | FSR_DZA | FSR_NXA)
 
-typedef uint64_t insn_bits_t;
 class insn_t
 {
 public:
-  insn_t() = default;
-  insn_t(insn_bits_t bits) : b(bits) {}
-  insn_bits_t bits() { return b; }
-  int64_t i_imm() { return int64_t(b) >> 20; }
-  int64_t s_imm() { return x(7, 5) + (xs(25, 7) << 5); }
-  int64_t sb_imm() { return (x(8, 4) << 1) + (x(25,6) << 5) + (x(7,1) << 11) + (imm_sign() << 12); }
-  int64_t u_imm() { return int64_t(b) >> 12 << 12; }
-  int64_t uj_imm() { return (x(21, 10) << 1) + (x(20, 1) << 11) + (x(12, 8) << 12) + (imm_sign() << 20); }
-  uint64_t rd() { return x(7, 5); }
-  uint64_t rs1() { return x(15, 5); }
-  uint64_t rs2() { return x(20, 5); }
-  uint64_t rs3() { return x(27, 5); }
-  uint64_t rm() { return x(12, 3); }
-  uint64_t csr() { return x(20, 12); }
+  uint32_t bits() { return b; }
+  int32_t i_imm() { return int32_t(b) >> 20; }
+  int32_t s_imm() { return x(7, 5) + (xs(25, 7) << 5); }
+  int32_t sb_imm() { return (x(8, 4) << 1) + (x(25,6) << 5) + (x(7,1) << 11) + (imm_sign() << 12); }
+  int32_t u_imm() { return int32_t(b) >> 12 << 12; }
+  int32_t uj_imm() { return (x(21, 10) << 1) + (x(20, 1) << 11) + (x(12, 8) << 12) + (imm_sign() << 20); }
+  uint32_t rd() { return x(7, 5); }
+  uint32_t rs1() { return x(15, 5); }
+  uint32_t rs2() { return x(20, 5); }
+  uint32_t rs3() { return x(27, 5); }
+  uint32_t rm() { return x(12, 3); }
+  uint32_t csr() { return x(20, 12); }
 private:
-  insn_bits_t b;
-  uint64_t x(int lo, int len) { return (b >> lo) & ((insn_bits_t(1) << len)-1); }
-  uint64_t xs(int lo, int len) { return int64_t(b) << (64-lo-len) >> (64-len); }
-  uint64_t imm_sign() { return xs(63, 1); }
+  uint32_t b;
+  uint32_t x(int lo, int len) { return b << (32-lo-len) >> (32-len); }
+  uint32_t xs(int lo, int len) { return int32_t(b) << (32-lo-len) >> (32-len); }
+  uint32_t imm_sign() { return xs(31, 1); }
 };
 
 template <class T, size_t N, bool zero_reg>
@@ -79,8 +76,18 @@ public:
   }
   void write(size_t i, T value)
   {
-    if (!zero_reg || i != 0)
-      data[i] = value;
+    data[i] = value;
+    if (zero_reg)
+      data[0] = 0;
+  }
+  void write(size_t i, T value, T tag) 
+  {
+    write(i, value);
+    tags[i] = tag;
+  }
+  T& read_tag(size_t i)
+  {
+    return tags[i];
   }
   const T& operator [] (size_t i) const
   {
@@ -88,6 +95,7 @@ public:
   }
 private:
   T data[N];
+  T tags[N];
 };
 
 // helpful macros, etc
@@ -120,6 +128,8 @@ private:
       })
 #endif
  
+
+
 #define SHAMT (insn.i_imm() & 0x3F)
 #define BRANCH_TARGET (pc + insn.sb_imm())
 #define JUMP_TARGET (pc + insn.uj_imm())
@@ -155,7 +165,11 @@ private:
    ((x) & 0x3f) < 0x3f ? 6 : \
    8)
 
-#define set_pc(x) (npc = sext_xprlen(x))
+#define set_pc(x) \
+  do { if ((x) & 3 /* For now... */) \
+         throw trap_instruction_address_misaligned(); \
+       npc = sext_xprlen(x); \
+     } while(0)
 
 #define validate_csr(which, write) ({ \
   unsigned my_priv = (STATE.sr & SR_S) ? 1 : 0; \
