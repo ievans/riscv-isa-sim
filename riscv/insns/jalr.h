829c2546058995a98a4788d03bb4560a17e8ca52
Isaac Evans <ine@mit.edu>
2015-01-19 17:57:15 -0500

    moved to header file

--- riscv/insns/jalr.h
+++ riscv/insns/jalr.h
@@ -1,8 +1,9 @@
 reg_t tmp = npc;
 // check the tag on the jump target to make sure it is ok to execute 
 // but only if RS1 is ra, the conventional return pointer register
-if (TAG_ENFORCE_ON && (!(TAG_S1 & TAG_PC)) && (insn.rs1() == 1)) {
-  throw trap_tag_violation();
+if ((1 || TAG_ENFORCE_ON) && (!(TAG_S1 & TAG_PC)) && (insn.rs1() == 1)) {
+    printf("trap would have happened at pc %08lx: \n", npc);
+//  throw trap_tag_violation();
 }
 set_pc((RS1 + insn.i_imm()) & ~reg_t(1));
 WRITE_RD_AND_TAG(tmp, TAG_PC);
--- test/hello.c
+++ test/hello.c
@@ -1,19 +1,21 @@
 int main();
 
+#include "tag-extensions.h"
+
 int g() {
-	return 4;
+    return 4;
 }
 
 int f() {
-	int x[10];
-	int i;
-	g();
-	x[12] = (0xdeadbeef);
-	return 0;
+    int x[10];
+    int i;
+    g();
+    x[12] = (0xdeadbeef);
+    return 0;
 }
 
 int main() {
-	__asm__ ("tagenforce ra,1");
-	f();
-	return 0;
+    tag_enforcement_on();
+    f();
+    return 0;
 }
