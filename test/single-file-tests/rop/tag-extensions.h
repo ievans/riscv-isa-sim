ce4c37fde313bd66ad053fa34ff8d9f3fff0d1b2
Julian Gonzalez <jugonz97@mit.edu>
2015-02-13 14:15:34 -0500

    In-progress malloc/free wrappers
    need to bind to relevant registers to get it to actually work

--- test/single-file-tests/rop/tag-extensions.h
+++ test/single-file-tests/rop/tag-extensions.h
@@ -1,33 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define NOT_AVAILABLE "warning: tag enforcement not available for this architecture"
+
 void tag_enforcement_on() {
 #ifdef __riscv
-    __asm__ ("tagenforce ra,1");
+  __asm__ ("tagenforce ra,1");
 #else
-    printf("warning: tag enforcement not available for this architecture");
+  printf(NOT_AVAILABLE);   
 #endif
 }
 
 // For now...
 void tag_enforcement_off() {
 #ifdef __riscv
-	__asm__ ("tagenforce ra,0");
+  __asm__ ("tagenforce ra,0");
 #else
-    printf("warning: tag enforcement not available for this architecture");
+  printf(NOT_AVAILABLE);
 #endif
 }
 
 // I don't understand GCC inline assembly...
 void set_tag_t3() {
 #ifdef __riscv
-	__asm__ ("settag t3,537");
+  __asm__ ("settag t3,537");
 #else
-    printf("warning: tag enforcement not available for this architecture");
+  printf(NOT_AVAILABLE);
 #endif
 }
 
 void clear_tag_t3() {
 #ifdef __riscv
-	__asm__ ("settag t3,0");
+  __asm__ ("settag t3,0");
 #else
-    printf("warning: tag enforcement not available for this architecture");
+  printf(NOT_AVAILABLE);
 #endif
 }
+
+void* malloc_tagged(size_t size, bool is_fptr) {
+  void* mem = malloc(size);
+
+  // Check for null ptrs.
+  if (!mem)
+    return NULL;
+
+#ifdef __riscv
+  // Return register is a0, mem should be there.
+  if (is_fptr) {
+    __asm__ __volatile__ ("settag a5, 2");
+  } else {
+    __asm__ __volatile__ ("settag a5, 1");
+  }
+#else
+  printf(NOT_AVAILABLE);
+#endif
+
+  return mem;
+}
+
+void free_tagged(void* ptr) {
+  // Check for null ptrs.
+  if (!ptr)
+    return;
+
+#ifdef __riscv
+  // No matter the type of pointer, set to uninitialized.
+  __asm__ __volatile__ ("settag a5, 0");
+#else
+  printf(NOT_AVAILABLE);
+#endif
+
+  free(ptr);
+}
+
+
