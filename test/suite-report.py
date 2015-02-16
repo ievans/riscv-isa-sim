a08bae7a6122524061faef5ebc48cedab6de216f
Isaac Evans <ine@mit.edu>
2015-02-16 14:05:24 -0500

    initial test suite implementation (ported from 035)

---
+++ test/suite-report.py
@@ -0,0 +1,158 @@
+#!/usr/bin/python
+
+# 6.035 Test Suite
+# Isaac Evans, Spring 2013
+# ~ a pure python replacement ~
+
+# sorry this is such a hackjob mixing html with py; I wrote it on a plane so I
+# didn't have access to any static templating libraries. oh well, it works
+
+import os
+import pickle
+import collections
+import subprocess
+
+def shell(command): 
+    p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    output = [x for x in p.stdout.readlines()]
+    retval = p.wait()
+    return output, retval
+
+gitMessages = {}
+def gitInfo():
+    output, retval = shell('git log --pretty=oneline --abbrev-commit')
+    if retval == 0:
+        for line in output:
+            h, msg = (line[:line.index(' ')], line[line.index(' '):].strip())
+            gitMessages[h] = msg
+
+gitInfo()
+
+baseURL = 'https://github.com/6035/keurig/commit/'
+baseFileURL = 'https://github.com/6035/keurig/blob/master/tests/'
+
+resultsBySHA = collections.OrderedDict()
+resultsBySHA = pickle.load(open('tests.dat'))
+
+# reverse the order of the keys so that the HEAD commit is on the left
+# resultsBySHA = collections.OrderedDict(reversed(resultsBySHA.items()))
+
+resultsByTest = {}
+for h in resultsBySHA.keys():
+    for testName in resultsBySHA[h].keys():
+        if testName not in resultsByTest:
+            resultsByTest[testName] = {}
+        for fileName in resultsBySHA[h][testName].keys():
+            if fileName in resultsByTest[testName]:
+                resultsByTest[testName][fileName].extend([(h, resultsBySHA[h][testName][fileName])])
+            else:
+                resultsByTest[testName][fileName] = [(h, resultsBySHA[h][testName][fileName])]
+
+# and now let's convert that output to HTML!
+f = open('output.html', 'w')
+f.write('''<html><head><script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+<style>
+body { font-family: monospace; font-size: 10pt; }
+.ok { background-color: #55FF33; }
+.positive { background-color: #55FF33; }
+.negative { background-color: red; color: white }
+.fail { background-color: red; color: white; }
+.category { background-color: black; color: white; vertical-align: bottom }
+.state { display: block; }
+.lines { display: none; }
+.cycles { display: none; }
+.linesDelta { display: none; }
+.cyclesDelta { display: none; }
+a .category { color: white}
+table { border-color: #600; border-width: 1px 1px 1px 1px; border-style: solid; }
+td { border-color: lightgray; border-width: 1px 1px 1px 1px; border-style: solid; }
+</style>
+<script type="text/javascript">                                         
+$(document).ready(function() {
+   $("#matrix").change(function() {
+     i = $(this).prop("selectedIndex");
+     $(".state").css("display", "none");
+     $(".lines").css("display", "none");
+     $(".linesDelta").css("display", "none");
+     $(".cycles").css("display", "none");
+     $(".cyclesDelta").css("display", "none");
+     names = ["state", "lines", "linesDelta", "cycles", "cyclesDelta"];
+     $("." + names[i]).css("display", "block");
+   });
+ });
+</script>
+<title>Keurig Test Suite Output</title></head><body><h3>Keurig Test Suite Output</h3>
+<select id="matrix"><option>correctness</option><option># instructions</option><option># instructions delta</option><option># microseconds</option><option># microseconds delta</option></select>''')
+
+f.write('<table>')
+
+isCorrect = lambda x: x[1][0][0]
+
+def writeSHAHeaderRow(f, headerText):
+    f.write('<tr class="category"><td>' + headerText + '</td>')
+    for i, h in enumerate(resultsBySHA.keys()):
+        f.write('<td>' + ('HEAD<br/>' if i == len(resultsBySHA.keys()) - 1 else '') + ' <a class="category" href="' + baseURL + h + '" title="' + (gitMessages[h] if h in gitMessages else '') + '">' + h + '</a>' + '</td>')
+    f.write('</tr>')
+
+writeSHAHeaderRow(f, 'overall')
+
+# overall
+for testName in sorted(resultsByTest.keys()):
+    f.write('<tr>')
+    f.write('<td>' + testName + '</td>')
+    for h in resultsBySHA.keys():
+        if not testName in resultsBySHA[h]:
+            f.write('<td>-</td>')
+            continue
+        allFiles = resultsBySHA[h][testName].values()
+        total = len(allFiles)
+        correct = len(filter(lambda x: x[0][0], allFiles))
+        state = 'ok'
+        if correct != total:
+            state = 'fail'
+        f.write('<td class="' + state + '">' + str(correct) + '/' + str(total)  + '</td>')
+    f.write('</tr>')
+
+# drilldown
+for testName in sorted(resultsByTest.keys()):
+    writeSHAHeaderRow(f, testName)
+
+    for fileName in sorted(resultsByTest[testName].keys()):
+        f.write('<tr><td><a href="' + baseFileURL + fileName.replace(',/', '') + '">' + os.path.basename(fileName) + '</a></td>')
+        prevCycles = 0
+        prevLines = 0
+        for h in resultsBySHA.keys():
+            found = None
+            linesDelta, cyclesDelta = '', ''
+            for item in resultsByTest[testName][fileName]:
+                if item[0] == h:
+                    found = item
+            if found == None: # if there is no data for a SHA, don't output anything
+                f.write('<td>-</td>')
+                continue
+            lines = found[1][0][1][0] if isCorrect(found) else 'fail'
+            cycles = found[1][0][1][1] if isCorrect(found) else 'fail'
+            if isCorrect(found) and lines != '' and cycles != '':
+                linesDelta = ("%+d" % (int(lines) - int(prevLines))).replace('+0', '')
+                cyclesDelta = ("%+d" % (int(cycles) - int(prevCycles))).replace('+0', '')
+                prevCycles = cycles
+                prevLines = lines
+            state = 'ok' if isCorrect(found) else 'fail'
+            divWrap = lambda text, cssclass: '<div class="' + cssclass + '">' + text + '</div>'
+            divWrap2 = lambda text, cssclass, cssclass2: '<div class="' + cssclass + ' ' + cssclass2 + '">' + text + '</div>'
+            def classForNumber(x):
+                if x == '': return ''
+                if x[0] == '+': return 'positive'
+                if x[0] == '-': return 'negative'
+
+            f.write('<td class="' + state + '">' + \
+                        divWrap(state, "state") +  \
+                        divWrap(lines, "lines") + \
+                        divWrap2(linesDelta, "linesDelta", classForNumber(linesDelta)) + \
+                        divWrap(cycles, "cycles") + \
+                        divWrap2(cyclesDelta, "cyclesDelta", classForNumber(cyclesDelta)) + \
+                        '</td>')
+        f.write('</tr>')
+
+f.write('</table></body></html>')
+f.close()
---
+++ test/suite-test.py
@@ -0,0 +1,152 @@
+#!/usr/bin/python
+
+# derived from the 6.035 Test Suite written by Isaac Evans, Spring 2013
+
+# ~ a pure python replacement ~
+
+import collections
+import pickle
+import tempfile
+import subprocess
+import sys
+import os
+
+# We assume we have compiled a set of executable statements (very generically!
+# could be compiler invocations, program invocations, etc), and we want to
+# exercise them and discover whether they match the previous outputs marked as
+# correct ("golden" outputs)
+
+# For convenience, the exeuctable statments may be exeuctable files, or simply
+# files. If they are files, the CC environment variable will be used to compile
+# them. Golden outputs have the same name as the executable file or file they
+# are derived from with the addition of a suffix:
+expected_output_suffix = '.out'
+
+# pre-execution action is also possible
+execution_prefix = 'spike pk'
+
+# We assume that executables that are supposed to segfault, return non-zero
+# values, etc., will be handled by a wrapper script that outputs that value in
+# such a way that it can be compared to the golden output
+
+# Modes are: 
+# (a) compare output to golden output
+# (b) track size of output 
+# (c) track execution time to produce output 
+
+make_gold = False
+if len(sys.argv) >= 2:
+    make_gold = sys.argv[1] == 'gold'
+
+ccompiler = os.environ.get('CC')
+cflags = os.environ.get('CFLAGS')
+cflags = cflags if cflags != None else ''
+
+tests = {
+          'single-file-tests': [('single-file-tests/bin/riscv', 'single-file-tests')],
+        }
+
+def shell(command):
+    print '[shell]', command
+    p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    output = [x for x in p.stdout.readlines()]
+    retval = p.wait()
+    return output, retval
+
+def getSHA(): 
+    return shell('git rev-parse --short HEAD')[0][0].strip()
+
+resultsBySHA = collections.OrderedDict()
+sha = getSHA()
+
+if os.path.exists('tests.dat'):
+    f = open('tests.dat')
+    resultsBySHA = pickle.load(f)
+    f.close()
+    print '[info] loaded previous result data from tests.dat'
+    if sha in resultsBySHA:
+        print '[info] tests have already been run for rev ' + sha + '; rerun tests? [y/n] ',
+        ans = raw_input()
+        if ans != 'y' and ans != 'Y':
+            sys.exit(0)
+    else: # initialize
+        resultsBySHA[sha] = collections.OrderedDict()
+else:
+    # clear everything out - or not; since the tests are individually done
+    resultsBySHA[sha] = collections.OrderedDict()
+
+def logInstrumentedSuccess(testName, fileName, assemblyFile, cycles):
+    # count the number of lines in the output file
+    lines = shell('wc -l < ' + assemblyFile)[0][0].strip()
+    # number of cycles it took to execute
+    instruments = [lines, cycles]
+    logFailure(testName, fileName, instruments, True)
+
+def logSuccess(testName, fileName):
+    logFailure(testName, fileName, ['', ''], True)
+
+def logFailure(testName, fileName, message, status = False):
+    if len(message) > 0 and type(message) == type(''):
+        print '[' + fileName + '] ' + message
+    if fileName not in resultsBySHA[sha][testName]:
+        resultsBySHA[sha][testName][fileName] = [(status, message)]
+    else:
+        resultsBySHA[sha][testName][fileName].extend([(status, message)])
+
+root_dir = os.getcwd()
+for test_name, options in tests.items():
+    inputDir, outputDir = map(lambda x: os.path.join(root_dir, x), list(options[0]))
+    resultsBySHA[sha][test_name] = {}
+    print '[info] running ' + test_name + ' tests (folder: ' + inputDir + ')'
+
+    for directory, subdirectories, files in os.walk(inputDir):
+        for file in files:
+            expected = os.path.join(outputDir, file + expected_output_suffix)
+            input =  os.path.join(directory, file)
+            binary = tempfile.NamedTemporaryFile()
+            output = tempfile.NamedTemporaryFile()
+
+            is_executable = os.access(input, os.X_OK)
+            if ccompiler == None and not is_executable:
+                logFailure(test_name, input, 'no compiler is defined, but the input file is not executable')
+                continue
+
+            if ccompiler != None:
+                compiler_call = '%s %s -o %s %s' % (ccompiler, cflags, binary.name, input)
+                error_output, retval = shell(compiler_call)
+                if retval != 0:
+                    logFailure(test_name, input, 'compiler gave nonzero return code ' + str(retval) + ' for command ' + compiler_call)
+                    continue
+                if len(error_output) > 0:
+                    logFailure(test_name, input, 'unexpected output ' + ''.join(error_output))
+                    continue
+
+            # also redirect stderr to stdout (2>&1)
+            exe_call = '%s %s > %s 2>&1' % (execution_prefix, input if is_executable else binary.name, output.name)
+            exe_error, exe_ret_val = shell(exe_call)
+
+            if exe_ret_val != 0:
+                logFailure(test_name, input, 
+                           'program failed to run (command "%s" returned %d): %s' %
+                           (exe_call, exe_ret_val, ''.join(exe_error)))
+                continue
+
+            if make_gold:
+                # copy the output to the golden reference
+                diffOutput, diffRetVal = shell('cp %s %s' % (output.name, expected))
+            else:
+                # run diff on the output
+                diffCall = 'diff -u %s %s' % (output.name, expected)
+                diffOutput, diffRetVal = shell(diffCall)
+                if diffRetVal != 0:
+                    logFailure(test_name, input, 'program output did not match expected output ' + ''.join(diffOutput))
+                    continue
+
+            cycles = 0
+            logInstrumentedSuccess(test_name, input, output.name, cycles)
+
+            binary.close()
+            output.close()
+
+# done! let's write out the output 
+pickle.dump(resultsBySHA, open('tests.dat', 'w'))
