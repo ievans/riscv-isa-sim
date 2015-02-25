7091092b91380eb7166acaaa05a031b6e5ae5b06
Isaac Evans <ine@mit.edu>
2015-02-25 18:39:51 -0500

    pull back changes to sb.h

--- riscv/insns/sb.h
+++ riscv/insns/sb.h
@@ -1,10 +1,1 @@
-tag_t tag = TAG_S2;
-
-#ifdef TAG_POLICY_NO_RETURN_COPY
-// If we're storing the value from RA into memory, clear the tag 
-if (TAG_ENFORCE_ON && insn.rs2() == RETURN_REGISTER) {
-    tag = CLEAR_RA_TAG(tag);
-}
-#endif
-
-MMU.store_tagged_uint8(RS1 + insn.s_imm(), RS2, tag);
+MMU.store_tagged_uint8(RS1 + insn.s_imm(), RS2, TAG_S2);
