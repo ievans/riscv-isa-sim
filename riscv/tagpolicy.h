385c93bbd6f5a8bf44f7ff3eae67c03311fd74a4
Julian Gonzalez <jugonz97@mit.edu>
2015-02-26 16:32:51 -0500

    Added CLEAR_PC_TAG wrapper to tag policies.
    TAG_ADD, TAG_SUB, TAG_ARITH & TAG_LOGIC now pass
    their tag through CLEAR_PC_TAG() before returning
    if TAG_POLICY_NO_RETURN_COPY is enabled.
    This prevents propagating tags through registers.
    (does not break explicit instructions like settag)

--- riscv/tagpolicy.h
+++ riscv/tagpolicy.h
@@ -54,10 +54,22 @@ void print_tag_policy() {
 }
 */
 
-#define TAG_ADD(tag1, tag2) ((tag1) ^ (tag2))
-#define TAG_SUB(tag1, tag2) ((tag1) ^ (tag2))
-#define TAG_ARITH(tag1, tag2) ((tag1) & (tag2))
-#define TAG_LOGIC(tag1, tag2) ((tag1) & (tag2))
+#ifdef TAG_POLICY_NO_RETURN_COPY
+#define CLEAR_PC_TAG(tag) 0
+#define CLEAR_TAG(reg, tag) WRITE_REG_TAG(reg, CLEAR_PC_TAG(tag))
+#endif // TAG_POLICY_NO_RETURN_COPY
+
+#ifdef TAG_POLICY_NO_RETURN_COPY
+  #define TAG_ADD(tag1, tag2) CLEAR_PC_TAG(((tag1) ^ (tag2)))
+  #define TAG_SUB(tag1, tag2) CLEAR_PC_TAG(((tag1) ^ (tag2)))
+  #define TAG_ARITH(tag1, tag2) CLEAR_PC_TAG(((tag1) & (tag2)))
+  #define TAG_LOGIC(tag1, tag2) CLEAR_PC_TAG(((tag1) & (tag2)))
+#else
+  #define TAG_ADD(tag1, tag2) ((tag1) ^ (tag2))
+  #define TAG_SUB(tag1, tag2) ((tag1) ^ (tag2))
+  #define TAG_ARITH(tag1, tag2) ((tag1) & (tag2))
+  #define TAG_LOGIC(tag1, tag2) ((tag1) & (tag2))
+#endif // TAG_POLICY_NO_RETURN_COPY
 
 #define TAG_ADD_IMMEDIATE(tag1) (tag1)
 #define TAG_SUB_IMMEDIATE(tag1) (tag1)
@@ -68,10 +80,5 @@ void print_tag_policy() {
 #define TAG_PC 1
 #define TAG_NULL 0
 #define TAG_IMMEDIATE 0
-
-#ifdef TAG_POLICY_NO_RETURN_COPY
-#define CLEAR_PC_TAG(tag) 0
-#define CLEAR_TAG(reg, tag) WRITE_REG_TAG(reg, CLEAR_PC_TAG(tag))
-#endif
 
 #endif // TAG_POLICY_H
