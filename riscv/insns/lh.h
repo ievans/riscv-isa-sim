ee6e92d0855cf20b8c04a096aa25aee5d65aafcb
Julian Gonzalez <jugonz97@mit.edu>
2015-03-10 13:38:57 -0400

    Now PC_TAG is cleared from non-ra regs on load.
    Only done if CLEAR_PC_TAG(tag) != tag.
    Notable: this creates trap messages on fork().

--- riscv/insns/lh.h
+++ riscv/insns/lh.h
@@ -1,14 +1,18 @@
 reg_t addr = RS1 + insn.i_imm();
 tagged_reg_t v = MMU.load_tagged_int16(addr);
 #ifdef TAG_POLICY_NO_RETURN_COPY
-tag_t tag = v.tag;
+// Clear PC_TAG from the memory location if present.
+tag_t cleared_tag = CLEAR_PC_TAG(v.tag);
+if (cleared_tag != v.tag) {
+  MMU.store_tag_value(cleared_tag, addr);
+}
 if ((TAG_ENFORCE_ON)
   && (insn.rd() != RETURN_REGISTER)
 )
-{
-  // should we clear this tag?
-  // CLEAR_TAG(insn.rd(), v.tag);
-  MMU.store_tag_value(CLEAR_PC_TAG(tag), addr);
+{ 
+  // Loading into a non-return register,
+  // must clear PC tag in destination register.
+  v.tag = cleared_tag;
 }
-#endif
+#endif // TAG_POLICY_NO_RETURN_COPY
 WRITE_RD_AND_TAG(v.val, v.tag);
