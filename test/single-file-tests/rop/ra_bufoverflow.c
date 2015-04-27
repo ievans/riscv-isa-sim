723c43e0c82557813350c230bc35853492da61e5
Julian Gonzalez <jugonz97@mit.edu>
2015-04-27 17:16:04 -0400

    New traditional buffer overflow test
    We didn't actually have a boring regular
    buffer overflow test (that uses a valid address),
    and now we do.
    Also removed demo3 (was just a plain malloc/free)
    and updated gitignore.

---
+++ test/single-file-tests/rop/ra_bufoverflow.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <setjmp.h>
+#include "../../tag-extensions.h"
+
+// This test executes a simple stack buffer overflow on
+// a return address, calling setjmp afterwards to see if
+// setjmp allows tag violations.
+// On RISCV-tagged with correct linearity this should throw a trap.
+// Note: on gcc you may need to use the -fno-stack-protector flag
+// for this test to actually work.
+
+#define BADSTR  "to win the game you must kill me, john romero.\n"
+
+uint64_t globalBuf[4];
+jmp_buf jmpb;
+
+void usage(void)
+{
+  printf("desired output is:\n");
+  printf("no output, and a tag trap\n");
+  printf("-------------------------\n");
+}
+
+void evilFunc(void)
+{
+  printf(BADSTR);
+}
+
+void test(void)
+{
+  // Small buffer to overflow.
+  char buf[8];
+
+  // Get the addr of our evil func.
+  globalBuf[3] = (uint64_t) &evilFunc;
+  // Some other garbage.
+  globalBuf[0] = 1337;
+  
+  // Do the buffer overwrite (size is too large here).
+  memcpy(buf, globalBuf, sizeof(globalBuf));
+  
+  // We should not return to the evil function.
+}
+
+int main(void)
+{
+  // Print usage message.
+  usage();
+
+  tag_enforcement_on();
+
+  test();
+
+  tag_enforcement_off(); // only if no tag trap
+
+  return 0;
+}
