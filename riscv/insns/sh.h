72844f7ee20582d47d0b18b7465799a1104cc22f
Sam Fingeret <samfin@mit.edu>
2015-05-11 17:35:19 -0400

    Add traps to store instructions

--- riscv/insns/sh.h
+++ riscv/insns/sh.h
@@ -1,10 +1,16 @@
 reg_t tag = TAG_S2;
+reg_t addr = RS1 + insn.s_imm();
 
 #ifdef TAG_POLICY_NO_PARTIAL_COPY
 tag = TAG_DATA;
+
+if((TAG_S1 & TAG_DATA) && !(addr & 0x8000000000000000L)) {
+    printf("halfword store trap at addr %016" PRIx64 ", pc %08lx: \n", addr, npc-4);
+    MMU.monitor();
+}
 #endif
 
-MMU.store_tagged_uint16(RS1 + insn.s_imm(), RS2, TAG_S2);
+MMU.store_tagged_uint16(addr, RS2, tag);
 // If we're storing the return address into memory...
 #ifdef TAG_POLICY_NO_RETURN_COPY
 if ((TAG_ENFORCE_ON)
