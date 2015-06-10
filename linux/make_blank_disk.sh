034bed698aa993aa22e06fa6e9750b18eccace7e
Sam Fingeret <samfin@mit.edu>
2015-06-10 14:40:29 -0400

    Create disk_setup scripts that don't use sudo

---
+++ linux/make_blank_disk.sh
@@ -0,0 +1,7 @@
+# stop if any command returns nonzero
+set -e
+
+echo "Creating disk image: blankdisk.bin"
+rm -f --preserve-root blankdisk.bin
+dd if=/dev/zero of=blankdisk.bin bs=1M count=64
+sudo mkfs.ext2 -F blankdisk.bin
